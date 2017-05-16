938035d4171ab0589caf291cf471a7a627fca2bd
Matthieu Marinangeli <matthieu.marinangeli@epfl.ch>
2017-05-16 14:36:40 +0200

    Faster Treatment with root_numpy and add fraction

--- Tree.py
+++ Tree.py
@@ -11,37 +11,56 @@ from PyLHCb.Utilities.Debug import memory_usage
 import glob
 import os
 import math
+import numpy as np
+from root_numpy import array2tree, tree2array, root2array, array2root
+from rootpy.tree import TreeChain
 
 
-
-def readTree(file,selection='',treename='DecayTree'):
+def readTree(file,selection='',treename='DecayTree',fraction=1):
     #doesn't work if a TFile is open
     if not file:
         raise ValueError('No file!!!')
     if not isinstance(file,(list,tuple)):
         file = [file]
     chain = ROOT.TChain(treename)
+#    chain = TreeChain(treename,file)
     for file in file:
         if not os.path.isfile(file):
             #doesn't like path with $HOME for example
             continue
         chain.AddFile(file)
-    return chain.CopyTree(selection)
-
-
-def makeTree(files,treename,fileoutput,selection=''):
+    tree = chain.CopyTree(selection)
+        
+    if fraction == 1:
+        return tree
+    else:
+        return FracOfTree(tree,fraction)
     
-    #tree = readTree(files,selection,treename)
     
-    #-->
-    file_out = ROOT.TFile(fileoutput,'RECREATE')
-    #tree_out = tree.CloneTree()
-    tree_out = readTree(files,selection,treename)
-    print tree_out.GetEntries()
-    tree_out.SetName('DecayTree')
-    tree_out.Write()
-    file_out.Write()
-    file_out.Close()
+def FracOfTree(tree,fraction=1):
+    
+    nEvts = tree.GetEntries()
+    new_nEvts = int(nEvts*fraction)
+    
+    array = tree2array(tree)
+    array = np.random.choice(array,new_nEvts)
+    
+    new_tree = array2tree(array,name=tree.GetName())
+    destruct_object(tree)
+    
+    return new_tree
+    
+def makeTree(files,treename,fileoutput,selection='',fraction=1.0,branches=None):
+    
+    tree_array = root2array(files,treename,branches,selection)
+
+    if not fraction == 1.0:
+        nEvts = int(len(tree_array)*fraction)
+        tree_array = np.random.choice(tree_array,nEvts)
+        
+    print len(tree_array)
+    
+    array2root(tree_array,fileoutput,'DecayTree','recreate')
 
 def Efficiency(tree_total,selection=''):
     
